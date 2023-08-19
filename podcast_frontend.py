3d2d6e8622fe19bdb556dbe4e28d38e658341f3b
Robert-Lewis3 <53926491+Robert-Lewis3@users.noreply.github.com>
2023-08-18 19:32:33 -0700

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -4,7 +4,7 @@ import json
 import os
 
 def main():
-    st.title('Newsletter Dashboard')
+    st.title('Welcome to the Weekly Podcast Roundup!')
 
     available_podcast_info = create_dict_from_json_files('.')
 
